cfb0a534f07045ebaee72fef00d55279df4e6523
王鹏飞 <1136040402@qq.com>
2021-09-02 11:13:47 +0800

    springboot

---
+++ src/test/java/com/dcits/project/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.dcits.project;
+
+import com.dcits.project.DemoApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.ParseException;
+
+@SpringBootTest(classes = DemoApplication.class)
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() throws ParseException {
+
+    }
+
+}
